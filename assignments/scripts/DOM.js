76aaaca909a6d87afac71e5cbd69e213778cdd30
George-Ariel <geo15006@byui.edu>
2019-03-13 17:24:44 -0600

    i had way to much fun hahaha

--- assignments/scripts/DOM.js
+++ assignments/scripts/DOM.js
@@ -1,7 +1,31 @@
-function poofPoofBuhBye() {
+
+  function cutToxicPeople() {
+    var item = document.getElementById("toxic");
+    item.parentNode.removeChild(item);
+  }
+
+
+  function poofPoofBuhBye() {
     var list = document.getElementById("loooongList");
     
     if (list.hasChildNodes()) {
       list.removeChild(list.childNodes[0]);
     }
+  }
+
+  function notToday() {
+    var theMILK = document.createElement("LI");
+    var textnode = document.createTextNode("MILK");
+    theMILK.appendChild(textnode);
+  
+    var list = document.getElementById("shoppingList");
+    list.insertBefore(theMILK, list.childNodes[0]);
+  }
+
+
+  function spookyJack() {
+    var listing = document.createElement("P");
+    var t = document.createTextNode("All work and no play makes Jack a dull boy.");
+    listing.appendChild(t);
+    document.body.appendChild(listing);
   }
