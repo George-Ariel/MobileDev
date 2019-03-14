b966034b77d0d8fd2ec43cfc7f58ff1da407b242
George-Ariel <geo15006@byui.edu>
2019-03-13 18:54:19 -0600

    my hoos are so boo'd

--- assignments/scripts/JShaha.js
+++ assignments/scripts/JShaha.js
@@ -1,17 +1,13 @@
 var theBeatles = ["John Lenon", "Paul McCartney", "Ringo Star"];
-document.getElementById("whoFOURTH").innerHTML = theBeatles;
-
 function beatlesWho() {
   theBeatles[theBeatles.length] = "George Harrison";
   document.getElementById("whoFOURTH").innerHTML = theBeatles;
 }
 
 
-var character = [];
-character[0] = "Shrek";
-character[1] = "Fiona";
-character[2] = "Donkey"; 
-character[3] = "Gingy";
-character[4] = "Lord Farquaad";
-document.getElementById("shrekMovies").innerHTML = character[0] + " " + character.length;
+var persona = ["a","b","c"];
+//persona['a'] = "Shrek";
+//persona['b'] = "Fiona";
+//persona['c'] = "Donkey"; 
+document.getElementById("shrekMovies").innerHTML = persona;
 
