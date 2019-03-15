fd23325e77ef9376b404d5de06c980300d8b2f1f
George-Ariel <geo15006@byui.edu>
2019-03-15 00:20:29 -0600

    do i hate my life? yup

--- assignments/scripts/movingMoon.js
+++ assignments/scripts/movingMoon.js
@@ -1,18 +1,3 @@
-HTML  CSS  JS Result
-EDIT ON
- var boxOne = document.getElementsByClassName('box')[0],
-    $boxTwo = $('.box:eq(1)');
-
-document.getElementsByClassName('toggleButton')[0].onclick = function() {
-  if(this.innerHTML === 'Play') 
-  { 
-    this.innerHTML = 'Pause';
-    boxOne.classList.add('horizTranslate');
-  } else {
-    this.innerHTML = 'Play';
-    var computedStyle = window.getComputedStyle(boxOne),
-        marginLeft = computedStyle.getPropertyValue('margin-left');
-    boxOne.style.marginLeft = marginLeft;
-    boxOne.classList.remove('horizTranslate');    
-  }  
-}
+$("#button").click(function() {
+  $('.transform').toggleClass('transform-active');
+});
