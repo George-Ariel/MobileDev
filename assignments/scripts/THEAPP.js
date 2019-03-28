671107c7e6baf0e76191c93b33fdb6c227525493
George-Ariel <geo15006@byui.edu>
2019-03-27 21:03:48 -0600

    YAYY added API to THEAPP #holla

---
+++ assignments/scripts/THEAPP.js
@@ -0,0 +1,22 @@
+// 35.6762° N, 139.6503° E - tokyo
+
+
+//getting JSON information
+function loadAPI() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        bringWeather(this.responseText);
+        // just to check --> console.log(JSON.parse(this.responseText));
+
+      }
+    };
+    xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?APPID=d81576e3aa1511ba13c1f7f6157ccf2d&lat=35&lon=139", true);
+    xhttp.send();
+  }
+
+  //geting the specific weather "subpoint" from the JSON
+  function bringWeather(data) {
+    var wrrr = JSON.parse(data);
+    console.log(wrrr.list[0].weather[0].main);
+  }
