dadaf34e5f39bdf275ed75c097abc9e045a65fd7
George-Ariel <geo15006@byui.edu>
2019-04-10 13:39:50 -0600

    clearly im missing something so BLANK SLATE FILE TO TEST

--- assignments/scripts/THEAPP.js
+++ assignments/scripts/THEAPP.js
@@ -1,40 +1,5 @@
 // 35.6762° N, 139.6503° E - tokyo
 
-// getting coordinates from user by allowing location
-var x = document.getElementById("coordinates");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition,showError);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
-}
-
- 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
-}
-
-
-// accounting for errors & users who have blocked their locations
-function showError(error) {
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
-      break;
-    case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
-      break;
-    case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
-      break;
-    case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
-      break;
-  }
-}
 
 
 
@@ -42,34 +7,31 @@ function showError(error) {
 
 
 //getting JSON information
-// function loadAPI() {
-//     var xhttp = new XMLHttpRequest();
-//     xhttp.onreadystatechange = function() {
-//       if (this.readyState == 4 && this.status == 200) {
-//         bringWeather(this.responseText);
-//         // just to check --> console.log(JSON.parse(this.responseText));
-//         // position.coords.latitude
-//         // position.coords.longitude
-//         show yoself
+function loadAPI() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        bringWeather(this.responseText);
+        // just to check --> console.log(JSON.parse(this.responseText));
 
-//       }
-//     };
-//     xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?APPID=d81576e3aa1511ba13c1f7f6157ccf2d&lat=35&lon=139", true);
-//     xhttp.send();
-//   }
+      }
+    };
+    xhttp.open("GET", "http://api.openweathermap.org/data/2.5/forecast?APPID=d81576e3aa1511ba13c1f7f6157ccf2d&lat=35&lon=139", true);
+    xhttp.send();
+  }
 
-//   //geting the specific weather "subpoint" from the JSON
-//   function bringWeather(data) {
-//     var wrrr = JSON.parse(data);
-//     console.log(wrrr.list[0].weather[0].main);
-//   }
+  //geting the specific weather "subpoint" from the JSON
+  function bringWeather(data) {
+    var wrrr = JSON.parse(data);
+    console.log(wrrr.list[0].weather[0].main);
+  }
 
 
 
 
   //grabbing the current city from the user - text not coordinates omg why did i take a break
 
-// localStorage.setItem("name", "uhhh");
-// document.getElementById('input').value = localStorage.getItem("name");
+localStorage.setItem("name", "uhhh");
+document.getElementById('input').value = localStorage.getItem("name");
 
 
