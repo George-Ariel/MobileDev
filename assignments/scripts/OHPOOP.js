3309d6d1b4d13fd68c07bdb70c47ae96b6171c05
George-Ariel <geo15006@byui.edu>
2019-04-13 09:18:53 -0700

    chuc chug along little sailor

--- assignments/scripts/OHPOOP.js
+++ assignments/scripts/OHPOOP.js
@@ -1,31 +1,59 @@
-var x = document.getElementById("coordinates");
-
-function getLocation() {
-  if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition, showError);
-  } else { 
-    x.innerHTML = "Geolocation is not supported by this browser.";
-  }
+function storeTime() {
+    var hhh = document.getElementById("hour").value;
+    localStorage.hour = hhh;
+    var mmm = document.getElementById("minutes").value;
+    localStorage.minutes = mmm;
 }
 
-function showPosition(position) {
-  x.innerHTML = "Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;
+// localStorage.WHATEVER is how you access saved data
+
+
+function disPlay() {
+  var out = "If you go to sleep at " + localStorage.hour + " : " + localStorage.minutes + " here are the times we recommend you go to sleep at.";
+  document.getElementById("showEm").innerHTML = out;
 }
 
-function showError(error) {
-  switch(error.code) {
-    case error.PERMISSION_DENIED:
-      x.innerHTML = "User denied the request for Geolocation."
-      break;
-    case error.POSITION_UNAVAILABLE:
-      x.innerHTML = "Location information is unavailable."
-      break;
-    case error.TIMEOUT:
-      x.innerHTML = "The request to get user location timed out."
-      break;
-    case error.UNKNOWN_ERROR:
-      x.innerHTML = "An unknown error occurred."
-      break;
-  }
+// information :
+// The average sleep cycle - 90 minutes long.
+// A typical night of sleep includes 5 full sleep cycles.
+// 90 x 5 = 450 minutes, or 7.5 hours.
+// Starting at your wake time, work back 7.5 hours to find your bedtime.
+
+// 6 cycles
+
+// 7 cycles 
+
+// 8 cycles
+
+// 4 cycles
+
+// 3 cycles
+
+
+
+// function sleepCalc {
+//     var hh = localStorage.hour;
+//     var mm = localStorage.minutes;
+
+// }
+  
+
+
+
+
+
+
+
+
+
+
+
+function aBout() {
+    var nomComplete = {};
+    nomComplete['nombre'] = document.getElementById("nombre").value;
+    nomComplete['country'] = document.getElementById("country").value;
+    localStorage.setItem("nomComplete", JSON.stringify(nomComplete));
+    var biogr = JSON.parse(localStorage.nomComplete);
+    var out = "Welcome " + biogr['nombre'] + " from " + biogr['country'] + "!";
+    document.getElementById("showName").innerHTML = out;
 }
